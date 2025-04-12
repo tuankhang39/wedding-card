2f2caa74588d6ad09225c94a2864bac284eb6628
Tuan Khang <khangletuan098@gmail.com>
2025-04-13 01:30:48 +0700

    add hash router fix bug deploy remove outlet

--- love-confession/src/main.tsx
+++ love-confession/src/main.tsx
@@ -3,11 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { HashRouter } from "react-router-dom";
-import config from "./config/envConfig.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename={config.BASE_PATH}>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
