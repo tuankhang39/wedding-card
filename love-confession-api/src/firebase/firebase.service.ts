63c5dcf92abfba48020da5d19266e34da5916673
Tuan Khang <khangletuan098@gmail.com>
2025-04-19 13:44:25 +0700

    Fix Firebase private key

--- love-confession-api/src/firebase/firebase.service.ts
+++ love-confession-api/src/firebase/firebase.service.ts
@@ -5,14 +5,8 @@ import * as admin from 'firebase-admin';
 @Injectable()
 export class FirebaseService implements OnModuleInit {
   onModuleInit() {
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
-    if (!privateKey) throw new Error('FIREBASE_PRIVATE_KEY missing');
     admin.initializeApp({
-      credential: admin.credential.cert({
-        clientEmail: process.env.CLIENT_EMAIL,
-        privateKey: privateKey,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-      }),
+      credential: admin.credential.cert('./firebase.json'),
       storageBucket: process.env.STORAGE_BUCKET,
     });
   }
